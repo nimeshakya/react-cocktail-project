b43a2fcefb6bd11f14c1cb1da1b39de41c90d54f
Nimesh Shakya <nimeshakyabtw@gmail.com>
2021-08-16 14:25:12 +0545

    initial commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useGlobalContext } from '../context';
+
+const SearchForm = () => {
+    const { setSearchTerm } = useGlobalContext();
+    const searchValue = React.useRef('');
+
+    const searchCocktail = () => {
+        setSearchTerm(searchValue.current.value);
+    };
+
+    React.useEffect(() => {
+        searchValue.current.focus();
+    }, []);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    return (
+        <section className='section search'>
+            <form className='search-form' onSubmit={handleSubmit}>
+                <div className='form-control'>
+                    <label htmlFor='name'>search yoru favourite cocktail</label>
+                    <input
+                        type='text'
+                        id='name'
+                        ref={searchValue}
+                        onChange={searchCocktail}
+                    />
+                </div>
+            </form>
+        </section>
+    );
+};
+
+export default SearchForm;
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section className='section about-section'>
+            <h2 className='section-title'>about us</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Architecto et dolorum modi hic odio? Exercitationem voluptatem
+                dicta reiciendis repellendus hic aperiam beatae praesentium
+                cumque provident ad, sequi neque ut iste placeat earum autem
+                assumenda explicabo. Impedit repellat dolorem odit ratione.
+            </p>
+        </section>
+    );
+};
+
+export default About;
